b953c8ba403d62e7a86d23f3a694831923850bb7
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-09-01 04:45:30 +0200

    hmmmm

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,56 @@
-use std::{collections::LinkedList, sync::Mutex, thread};
+use std::{
+    collections::LinkedList,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 use guugle::get_links;
 
-struct Link {
-    visited: bool,
+struct ChannelMessage {
     link: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // first is visited links, last is links that need visiting
-    let mut links: Mutex<(Vec<String>, LinkedList<String>)> = Mutex::new((
-        vec![],
-        LinkedList::from_iter(
-            [
-                "https://dominik-schwaiger.ch".to_string(),
-                "https://en.wikipedia.org/wiki/Social_media".to_string(),
-            ]
-            .into_iter(),
-        ),
-    ));
+    // 1. make mutex with all links that have to be visited
+    // 2. make list with all visited links
+    // 3. tx pops one new link from mutex, visits it and sends info to receiver, gets new link, repeat
+    // 4. rx gets all visited links, adds them to another list (in the future with other data)
+    // 5. rx adds all newly discovered links to the mutex
+
+    let to_visit = Arc::new(Mutex::new(LinkedList::<String>::new()));
+    let visited = Arc::new(Mutex::new(LinkedList::<String>::new()));
+
+    let (tx, rx) = mpsc::channel::<ChannelMessage>();
+
+    let mut handles = vec![];
 
     for _ in 0..20 {
-        thread::spawn(|| async {
-            let mut m = links.lock().unwrap();
-            let link = m.1.pop_front();
+        let tx_c = tx.clone();
+        let to_visit_c = Arc::clone(&to_visit);
+        let visited_c = Arc::clone(&visited);
+
+        handles.push(thread::spawn(|| async move {
+            let link = to_visit_c.lock().unwrap().pop_front();
 
             if let Some(link) = link {
-                let new_links = get_links(&link).await;
+                let links = get_links(&link).await;
+                visited_c.lock().unwrap().push_back(link);
 
-                for l in new_links {
-                    m.1.push_back(l);
+                for link in links {
+                    tx_c.send(ChannelMessage { link }).ok();
                 }
             }
-        });
+        }));
+    }
+
+    for message in rx {
+        to_visit.lock().unwrap().push_back(message.link);
+        println!("{:?}", visited);
+    }
+
+    for handle in handles {
+        handle.join();
     }
 
     Ok(())
