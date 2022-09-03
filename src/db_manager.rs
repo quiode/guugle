a3716f7936412f740b7555ae8a380585ff6781f5
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-09-03 15:44:46 +0200

    db can update visited links

--- src/db_manager.rs
+++ src/db_manager.rs
@@ -17,10 +17,10 @@ pub fn create_default_tables(
     conn.execute(
         "CREATE TABLE IF NOT EXISTS Ranking (
 	id INTEGER NOT NULL PRIMARY KEY,
+    visited BOOLEAN NOT NULL DEFAULT false CHECK (visited IN (0, 1)),
   	url TEXT NOT NULL,
   	content TEXT,
-  	links_to TEXT,
-  	page_rank integer);",
+  	links_to TEXT);",
         (),
     )?;
 
@@ -41,18 +41,18 @@ fn unvisited_page(conn: &DatabaseConnection, url: &str) -> Result<i64, rusqlite:
 }
 
 /// updates the database entry for the page to visited and fills in the required data
-/// the pagerank fiel and the from field gets calculated by the function itself
 fn update_to_visited(
     conn: &DatabaseConnection,
     id: i64,
     content: &str,
     links_to: Vec<&str>,
 ) -> Result<i64, rusqlite::Error> {
-    let mut statement = conn
-        .connection
-        .prepare("UPDATE Ranking SET content = ?1, links_to = ?2, page_rank = ?3 WHERE id = ?4;")?;
+    conn.connection.execute(
+        "UPDATE Ranking SET visited = true, content = ?1, links_to = ?2 WHERE id = ?3;",
+        [content, &links_to.join(":::"), &id.to_string()],
+    )?;
 
-    todo!()
+    Ok(id)
 }
 
 /// calculates how many pages point to this page
@@ -139,6 +139,71 @@ mod tests {
         assert_eq!(test_result1, 3);
 
         assert_eq!(test_result2, 1);
+    }
+
+    #[test]
+    fn test_update_to_visited() {
+        let path = gen_random_path();
+
+        let conn = create_default_tables(path.to_str().unwrap()).unwrap();
+
+        // fill db with values
+        let mut prep = conn
+            .connection
+            .prepare("INSERT INTO Ranking (url, links_to) VALUES (?1, ?2)")
+            .unwrap();
+
+        prep.execute(["test.ch", "team-crystal.ch:::google.ch:::example.com"])
+            .unwrap();
+        prep.execute(["help.ch", "team-crystal.ch:::google.ch:::test.ch"])
+            .unwrap();
+        prep.execute(["lp.ch", "help.ch"]).unwrap();
+        prep.execute(["ep.ch", "team-crystal.ch:::help.ch"])
+            .unwrap();
+        prep.execute(["lp.ch", "help.ch:::google.ch"]).unwrap();
+
+        let content = r#"
+            <!doctype html>
+                <html>
+                <head>
+                    <title>Example Domain</title>
+
+                    <meta charset="utf-8" />
+                    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </head>
+
+                <body>
+                <div>
+                    <h1>Example Domain</h1>
+                    <p>This domain is for use in illustrative examples in documents. You may use this
+                    domain in literature without prior coordination or asking for permission.</p>
+                    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
+                </div>
+                </body>
+                </html>
+        "#;
+
+        let links_to = vec!["ep.ch", "lp.ch"].join(":::");
+
+        // update value
+        update_to_visited(&conn, 1, content, vec!["ep.ch", "lp.ch"]).unwrap();
+
+        // test if values have been updatet
+        let row: (i64, String, String) = conn
+            .connection
+            .prepare("SELECT visited, content, links_to FROM Ranking WHERE id = ?1;")
+            .unwrap()
+            .query_row(["1"], |row| {
+                Ok((row.get_unwrap(0), row.get_unwrap(1), row.get_unwrap(2)))
+            })
+            .unwrap();
+
+        fs::remove_file(path).unwrap();
+
+        assert_eq!(row.0, 1);
+        assert_eq!(row.1, content.to_string());
+        assert_eq!(row.2, links_to);
     }
 
     fn gen_random_path() -> PathBuf {
